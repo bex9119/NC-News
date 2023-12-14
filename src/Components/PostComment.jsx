6aa55978f628200aa08a50d55b09cd4eaa300418
bex9119 <60200396+bex9119@users.noreply.github.com>
2023-12-14 10:44:42 +0000

    update error handling & disable submit button

--- src/Components/PostComment.jsx
+++ src/Components/PostComment.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { postComment } from "../api"
 import { UserContext } from "../Context/UserContext";
 
@@ -6,7 +6,7 @@ const PostComment = ({ article_id , setCommentsList}) => {
     const { user } = useContext(UserContext);
   const [isLoading, setIsLoading] = useState(false)
   const [isErr, setIsErr] = useState(false)
-
+const [isDisabled, setIsDisabled] = useState(false)
     const [newComment, setNewComment] = useState('')
     const [blankComment, setBlankComment] = useState({id: '', placeholder: 'Comment...'})
 
@@ -16,7 +16,8 @@ const PostComment = ({ article_id , setCommentsList}) => {
     const handleSubmit = (event) => {
         event.preventDefault();
       if (newComment) {
-          setIsLoading(true)
+        setIsLoading(true)
+        setIsDisabled(true)
           postComment(article_id, { username: user.username, body: newComment }).then((postedComment) => {
             setCommentsList((currComments) => {
                   return [postedComment, ...currComments]
@@ -24,11 +25,17 @@ const PostComment = ({ article_id , setCommentsList}) => {
               setNewComment('')
             setBlankComment({ id: '', placeholder: 'Comment...' }) 
             setIsLoading(false)
-          }).catch((err) => {
+            setIsDisabled(false)
+          }).catch(() => {
+            setTimeout(() => {
+              setIsErr(false)
+              setIsDisabled(false)
+              setIsLoading(false)
+            }, 2000)
               setIsErr(true)
             })
 
-        }
+      }
         else {
             setBlankComment({ id: "blankComment", placeholder: "Please enter a comment here." });
         }
@@ -45,9 +52,9 @@ const PostComment = ({ article_id , setCommentsList}) => {
           id={blankComment.id}
           placeholder={blankComment.placeholder}
         ></textarea>
-        <button>{isLoading ? "Please wait" : "Comment"}</button>
+        <button disabled={isDisabled}>{isLoading ? "Please wait" : "Comment"}</button>
       </form>
-        <p className="errorMsg">{isErr ? "Unable to commment, please try again later" : ""}</p>
+        <p className="errorMsg">{isErr ? "Unable to comment, please try again later" : ""}</p>
     </>
   );
 }
