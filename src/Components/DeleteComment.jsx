c54694a8e3178eb8d9c2944b262d165b3dafbd2d
bex9119 <60200396+bex9119@users.noreply.github.com>
2023-12-14 14:35:47 +0000

    remove optimistic rendering

--- src/Components/DeleteComment.jsx
+++ src/Components/DeleteComment.jsx
@@ -1,37 +1,47 @@
-import { useContext, useEffect, useState } from "react"
+import { useContext, useState } from "react";
 import { UserContext } from "../Context/UserContext";
 import DeleteSharpIcon from "@mui/icons-material/DeleteSharp";
 import IconButton from "@mui/material/IconButton";
 import { deleteComment } from "../api";
 
-const DeleteComment = ({commentAuthor, commentId, setCommentsList, multipleComments}) => {
-    const { user } = useContext(UserContext)
-    const [savedComments, setSavedComments] = useState([])
+const DeleteComment = ({ commentAuthor, commentId, setCommentsList }) => {
+  const { user } = useContext(UserContext);
+  const [isLoading, setIsLoading] = useState(false);
+    const [isErr, setIsErr] = useState(false);
+    
+  const removeComment = () => {
+    setIsLoading(true);
+    deleteComment(commentId)
+      .then(() => {
+        setIsLoading(false);
+        return setCommentsList((currComments) => {
+          return [...currComments].filter((comment) => {
+            return comment.comment_id !== commentId;
+          });
+        });
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setIsErr(true);
+      });
+  };
 
-    useEffect(() => {
-        setSavedComments(multipleComments)
-    }, [])
+  if (user.username === commentAuthor) {
+    return (
+      <IconButton
+        onClick={() => {
+          removeComment();
+        }}
+        style={{ color: "inherit", fontSize: "inherit" }}
+      >
+        <span>
+          {isLoading ? "Deleting..." : ""}
+          {isErr ? "Unable to delete, try again later" : ""}
+        </span>
+        <DeleteSharpIcon />
+      </IconButton>
+    );
+  }
+};
 
-    const removeComment = () => {
-        setCommentsList((currComments) => {
-                   return [...currComments].filter((comment) => {
-                       return comment.comment_id !== commentId
-                   })
-               })
-        deleteComment(commentId).catch(() => {
-            setCommentsList(savedComments)
-        })
-    }
-
-    if (user.username === commentAuthor) {
-        return (
-          <IconButton onClick={()=>{removeComment()}}
-            style={{ color: "inherit", fontSize: "inherit" }}
-            > 
-            <DeleteSharpIcon />
-          </IconButton>
-        );
-    }
-}
-
-export default DeleteComment
+export default DeleteComment;
