60d5ca0365f3701da5c902ad8ab748955fcfa074
bex9119 <60200396+bex9119@users.noreply.github.com>
2023-12-15 08:36:41 +0000

    add most commented and liked articles to home

--- src/Components/ArticleHome.jsx
+++ src/Components/ArticleHome.jsx
@@ -7,24 +7,32 @@ const ArticleHome = () => {
   const [articlesList, setArticlesList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [limitedArticles, setLimitedArticles] = useState([]);
+  const [mostCommentedArticle, setMostCommentedArticle] = useState([])
+  const [mostLikedArticle, setMostLikedArticle] = useState([])
 
   useEffect(() => {
       getAllArticles().then((allArticles) => {
           setArticlesList(allArticles)
           setIsLoading(false);
       })
+    getAllArticles(null, null, 'comment_count').then((commentArticles) => {
+      setMostCommentedArticle([commentArticles[0]])
+    });
+    getAllArticles(null, null, 'votes').then((voteArticles) => {
+      setMostLikedArticle([voteArticles[0]])
+    })
   }, [])
     
     useEffect(() => {
         setLimitedArticles([...articlesList].slice(0, 5));
     }, [articlesList])
 
-
   if (isLoading) {
       return <h2>Loading...</h2>
   }
   else {
-      return (
+    return (
+      <>
         <main>
           <h2>Newest</h2>
           <ArticleCard multipleArticles={limitedArticles} />
@@ -32,7 +40,14 @@ const ArticleHome = () => {
             <button>All Articles</button>
           </Link>
         </main>
-      );
+        <section>
+          <h2>Most Commented:</h2>
+          <ArticleCard multipleArticles={[...mostCommentedArticle]} />
+          <h2>Most Liked:</h2>
+          <ArticleCard multipleArticles={[...mostLikedArticle]} />
+        </section>
+      </>
+    );
   }
 }
 
